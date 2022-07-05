ec4f5ce3bc23a7e9bb74e7f712ff8eb4e9e1ea2d
“Akpughe” <davidakpughe2@gmail.com>
2022-07-05 14:16:29 +0200

    rewrite socket server

--- server.js
+++ server.js
@@ -1,23 +1,67 @@
-const PORT = process.env.PORT || 6000;
-const INDEX = '/index.html';
-const WebSocket = require('ws');
-const app = require('express')();
+// const PORT = process.env.PORT || 6000;
+// const INDEX = '/index.html';
+// const WebSocket = require('ws');
+// const app = require('express')();
 
-const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+// const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const { Server } = require('ws');
+// const { Server } = require('ws');
 
-// const wss = new WebSocket.Server({ port: 8080 });
-const wss = new Server({ server });
+// // const wss = new WebSocket.Server({ port: 8080 });
+// const wss = new Server({ server });
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(data) {
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data.toString());
-        console.log('data', data.toString());
-        // client.send(new Date().toTimeString());
+// wss.on('connection', function connection(ws) {
+//   ws.on('message', function incoming(data) {
+//     wss.clients.forEach(function each(client) {
+//       if (client !== ws && client.readyState === WebSocket.OPEN) {
+//         client.send(data.toString());
+//         console.log('data', data.toString());
+//         // client.send(new Date().toTimeString());
+//       }
+//     });
+//   });
+// });
+
+const webSocketsServerPort = 8000;
+const webSocketServer = require('websocket').server;
+const http = require('http');
+
+// Spinning the http server and the websocket server.
+const server = http.createServer();
+server.listen(webSocketsServerPort);
+console.log('listening on port 8000');
+
+
+const wsServer = new webSocketServer({
+  httpServer: server
+});
+
+const clients = {};
+
+// This code generates unique userid for everyuser.
+const getUniqueID = () => {
+  const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  return s4() + s4() + '-' + s4();
+};
+
+wsServer.on('request', function (request) {
+  var userID = getUniqueID();
+  console.log((new Date()) + ' Recieved a new connection from origin ' + request.origin + '.');
+
+  // You can rewrite this part of the code to accept only the requests from allowed origin
+  const connection = request.accept(null, request.origin);
+  clients[userID] = connection;
+  console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients));
+
+  connection.on('message', function(message) {
+    if (message.type === 'utf8') {
+      console.log('Received Message: ', message.utf8Data);
+
+      // broadcasting message to all connected clients
+      for(key in clients) {
+        clients[key].sendUTF(message.utf8Data);
+        console.log('sent Message to: ', clients[key]);
       }
-    });
-  });
+    }
+  })
 });

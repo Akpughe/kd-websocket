816ebd8852ddd68b3cb7252a09459471156de544
“Akpughe” <davidakpughe2@gmail.com>
2022-07-06 17:40:03 +0200

    remove  sendUTF

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ wsServer.on('request', function (request) {
 
       // broadcasting message to all connected clients
       for(key in clients) {
-        clients[key].sendUTF(message.utf8Data);
+        // clients[key].sendUTF(message.utf8Data);
         console.log('sent Message to: ', clients[key]);
       }
     }

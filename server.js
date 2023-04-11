6d72a273f943a4d4f90b26f7186872666e2d6838
“Akpughe” <davidakpughe2@gmail.com>
2023-04-11 21:46:33 +0100

    add start cmd

--- server.js
+++ server.js
@@ -1,27 +1,3 @@
-// const PORT = process.env.PORT || 6000;
-// const INDEX = '/index.html';
-// const WebSocket = require('ws');
-// const app = require('express')();
-
-// const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-// const { Server } = require('ws');
-
-// // const wss = new WebSocket.Server({ port: 8080 });
-// const wss = new Server({ server });
-
-// wss.on('connection', function connection(ws) {
-//   ws.on('message', function incoming(data) {
-//     wss.clients.forEach(function each(client) {
-//       if (client !== ws && client.readyState === WebSocket.OPEN) {
-//         client.send(data.toString());
-//         console.log('data', data.toString());
-//         // client.send(new Date().toTimeString());
-//       }
-//     });
-//   });
-// });
-
 const webSocketsServerPort = process.env.PORT || 8000;;
 const webSocketServer = require('websocket').server;
 const http = require('http');
